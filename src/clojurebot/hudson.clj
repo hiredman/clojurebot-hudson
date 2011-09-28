4ab495bebad7c4c3b1b6b272a421697a669d78d5
Kevin Downey <redchin@gmail.com>
2011-09-28 16:15:26 -0700

    support parameterized builds

--- src/clojurebot/hudson.clj
+++ src/clojurebot/hudson.clj
@@ -8,9 +8,10 @@
 (defn do-a-build [{:keys [message config] :as a-map}]
   (let [hudson (:hudson config)
         hudson (if (.endsWith hudson "/") hudson (str hudson "/"))
-        build-url (format "%sjob/%s/build?delay=0sec"
-                          hudson
-                          (last (re-find #"^(build) (.*)" message)))]
-    (:status
-     (http/get build-url
-               {:basic-auth (:hudson-credentials config)}))))
+        [build & opts] (.split (last (re-find #"^(build) (.*)" message)) " ")
+        build-url (if (seq opts)
+                    (format "%sjob/%s/buildWithParameters" hudson build)
+                    (format "%sjob/%s/build" hudson build))]
+    (http/get build-url {:basic-auth (:hudson-credentials config)
+                         :query-params (apply hash-map opts)})
+    (format "%s/job/%s" hudson build)))
